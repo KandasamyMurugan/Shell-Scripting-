2fe7c6c8e489db37d2b2d9568f8cfc6150fbd1de
Sriharsha Kalluru <sriharsha@thinkdeeply.com>
2023-12-30 08:19:39 +0530

    Shell Code

--- 05-exit.sh
+++ 05-exit.sh
@@ -1,3 +1,28 @@
+sample2() {
+  echo Hello1
+  return 100
+  echo Hello2
+}
+
+
+sample2
+echo Exit status of function - $?
+
+
 echo Hello1
-exit
+exit 200
 echo Hello2
+
+# Exit the script using exit command to command line
+# Exit the function using return command to main program
+
+# Both commands behaviour is same and it is exiting.
+# while exiting both the commands can hold some information and it can show that information.
+# That information is called the status. Also called as Exit Status
+# This information is a number from 0-255
+
+# Zero considers as success message
+# Non zero considers as failure message
+
+# We can check this information using a variable which is ? , Meaning $? can show exit status of return or exit command
+# return and exit command by default return value zero , However we can pass the value from o-255 to those commands and that value will be returned
