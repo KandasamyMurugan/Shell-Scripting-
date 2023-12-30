cbd0bebb299f59615e40b3841d5b978b4b04f539
Sriharsha Kalluru <sriharsha@thinkdeeply.com>
2023-12-30 08:01:06 +0530

    Shell Code

--- 04-function.sh
+++ 04-function.sh
@@ -1,5 +1,22 @@
+# Function
 sample1() {
+  # Inside function code
   echo "Hello World"
 }
 
+# Main Program
 sample1
+
+# Declared variable in main program can be accessed in function  and it is vice versa
+# Declared variable in main program can be overwriten by function and it is vice versa
+
+# Along with this function have special variables , We can pass arguments to function
+
+# sample1 100 200
+
+sample_sum() {
+  echo "Addtion of $1 and $2 is $(($1+$2))"
+}
+
+sample_sum 100 200
+
