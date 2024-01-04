d53d39fc05ff02b0d8504ce6338ef69c32a3ccac
Sriharsha Venkata Raghu Kalluru <raghuk.vit@gmail.com>
2024-01-04 08:00:45 +0530

    Shell Code

---
+++ 08-loops.sh
@@ -0,0 +1,27 @@
+# Two loop Commands  (2 additional commands)
+
+# While Loop
+
+# while [ expression ]; do
+  # commands
+# done
+# if you want to control a loop with an expression then while has to be choice
+
+# Say Hello World 10 times
+i=10
+while [ "$i" -gt 0 ]; do
+  echo Hello World
+  i=$(($i-1))
+done
+
+# For Loop
+# for somevar in values(value1 value2); do
+  # commands
+# done
+# If you want to control the loop based on number of values the for has to be the choice.
+
+# Print List of Course
+for course in DevOps AWS Azure GCP Testing; do
+  echo Welcome to Course - ${course}
+done
+
